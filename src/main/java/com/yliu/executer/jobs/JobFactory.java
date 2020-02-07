c9bc98cb651fe3fb8deb358aabeed58ca3c6ed1c
LiuYonghua <liuyonghua@ziasset.com>
2020-02-07 21:31:08 +0800

    增加任务

--- src/main/java/com/yliu/executer/jobs/JobFactory.java
+++ src/main/java/com/yliu/executer/jobs/JobFactory.java
@@ -14,6 +14,7 @@ public class JobFactory {
 	
 	static{
 		JOB_MAP.put(JobType.JOB_CRAWLER.getJobName(), JobType.JOB_CRAWLER);
+		JOB_MAP.put(JobType.VEDIO_CRAWLER.getJobName(), JobType.VEDIO_CRAWLER);
 	}
 	
 	public static void run(String jobName){
